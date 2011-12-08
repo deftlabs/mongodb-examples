37a903632aacbb558b1fa2ba8f54f56a2ce41644
Ryan <rgnitz@gmail.com>
2011-12-07 16:26:40 -0800

    initial load.

---
+++ example-template/src/main/com/deftlabs/examples/mongo/SampleExample.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright 2011, Deft Labs.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.deftlabs.examples.mongo;
+
+// Mongo
+import com.mongodb.*;
+
+// JUnit
+import org.junit.*;
+import static org.junit.Assert.*;
+
+// Java
+import java.util.*;
+
+/**
+ * A sample example :-)
+ */
+public final class SampleExample {
+
+    @Test
+    public void runThis() throws Exception {
+        System.out.println("Hello!");
+    }
+
+    @BeforeClass
+    public static void start() throws Exception {
+
+    }
+
+    @AfterClass
+    public static void stop() {
+
+    }
+
+}
+
